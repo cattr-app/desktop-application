e3e6fad5b745a5738af4108ad1dd9f61904ce73e
Dmitry Pepperstein <dmitry@amazingcat.net>
2020-07-06 17:19:18 +0300

    Configured artifacts & commits history in Sentry plugin

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const mix = require('laravel-mix');
+const SentryWebpackPlugin = require('@sentry/webpack-plugin');
 
 mix.setPublicPath('./build');
 mix.disableSuccessNotifications();
@@ -11,7 +12,7 @@ if (mix.inProduction)
 else
   mix.js('./app/renderer/js/app.js', 'app.js');
 
-/* Build Sass */
+/* Build SASS */
 mix
   .sass('./app/renderer/scss/app.scss', 'app.css')
   .options({ processCssUrls: false });
@@ -25,3 +26,30 @@ mix
     './node_modules/element-ui/packages/theme-chalk/lib/fonts/element-icons.woff',
     path.resolve(__dirname, 'build', 'fonts', 'element-icons.woff'),
   );
+
+/* If MAKE_RELEASE flag is set, build renderer in production mode, then submit all the code to Sentry */
+if (mix.inProduction && process.env.MAKE_RELEASE) {
+
+  // eslint-disable-next-line global-require
+  const packageManifest = require('./package.json');
+
+  mix.webpackConfig({
+    plugins: [
+      new SentryWebpackPlugin({
+        include: 'build',
+        ignore: ['mix-manifest.json'],
+        configFile: path.resolve(__dirname, '.sentry.renderer'),
+        release: `${packageManifest.name}@${packageManifest.version}`,
+        setCommits: { auto: true },
+      }),
+      new SentryWebpackPlugin({
+        include: 'app/src',
+        urlPrefix: 'app/src/',
+        configFile: path.resolve(__dirname, '.sentry.main'),
+        release: `${packageManifest.name}@${packageManifest.version}`,
+        setCommits: { auto: true },
+      }),
+    ],
+  });
+
+}
