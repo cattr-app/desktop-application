c2669546e2fe2828c8034c7f352c8bef113922d5
Dmitry Pepperstein <dmitry@amazingcat.net>
2020-12-23 16:14:55 +0300

    Fixes for task filtering and access restrictions on macOS

--- app/src/base/os-integration.js
+++ app/src/base/os-integration.js
@@ -272,28 +272,6 @@ class OSIntegration extends EventEmitter {
    */
   static async getTrackingAvailability() {
 
-    if (process.platform === 'darwin') {
-
-      // (macOS) Check is the accessibility permissions granted?
-      if (!systemPreferences.isTrustedAccessibilityClient(true)) {
-
-        log.warning('We don\'t have an accessibility permissions');
-        return { available: false, reason: 'macos_no_accessibility_permissions' };
-
-      }
-
-    } else if (process.platform === 'linux') {
-
-      // (linux) Check is Wayland is active?
-      if (process.env.XDG_SESSION_TYPE && process.env.XDG_SESSION_TYPE === 'wayland') {
-
-        log.warning('Wayland is not supported for tracking yet');
-        return { available: false, reason: 'wayland_is_not_supported' };
-
-      }
-
-    }
-
     return { available: true };
 
   }
